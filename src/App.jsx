2796357feb61045f52e9a6f85d1b6da60aba0e66
Gaurav Vijay JAdhav <gauravvjadhav1432@gmail.com>
2023-06-16 20:14:49 +0530

    display X on clicking on square

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import Board from './Components/Board';
 import './App.scss';
 
 function App() {
+  let counter = 1;
+
+  const onBtnClick = () => {
+    console.log('Hello');
+    counter = counter + 1;
+  };
+
   return (
     <div className="app">
       <Board />
